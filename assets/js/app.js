1f061d594e9bd25c3f663796d0197d2c8bd1f691
Alex Vohs <Alex Vohs>
2017-06-27 21:22:50 -0400

    adding contact me section

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,6 +45,28 @@ $(document).ready(function(){
   });
 });
 
+$("#contactLink").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  } // End if
+});
+
 $('.flip').click(function(){
      $(this).find('.card').addClass('flipped').mouseleave(function(){
          $(this).removeClass('flipped');
