afb36107c89e73717268728879a030b4707861b8
Mthomaswicher <mthomaswicher@gmail.com>
2017-08-07 22:34:32 -0400

    style changes

---
+++ assets/app.js
@@ -0,0 +1,88 @@
+// function animationClick(element, animation){
+//     element = $('#projectsLink');
+//     element.click(
+//         function() {
+//             element.addClass('animated ' + animation);
+//             //wait for animation to finish before removing classes
+//             window.setTimeout( function(){
+//                 element.removeClass('animated ' + animation);
+//             }, 2000);
+//
+//         });
+// }
+$(document).ready(function(){
+  $('.card').on('click', function(){
+    $(this).toggleClass('flipped');
+  })
+});
+
+
+$(document).ready(function(){
+
+  // Add smooth scrolling to all links
+  $("#aboutMeLink").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  $("#projectsLink").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+$("#contactLink").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  } // End if
+});
